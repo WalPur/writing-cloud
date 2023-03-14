a3fe2763597082579e5bef296b4939fc994021eb
ichail <mishkamba1@gmail.com>
2023-03-14 13:53:19 +0900

    Добавлена модель публикаций

--- backend/publication/apps.py
+++ backend/publication/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsersConfig(AppConfig):
+class PublicationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "users"
+    name = "publication"
---
+++ backend/publication/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Publication(models.Model):
+    class Genres(models.TextChoices):
+        CO = "CO", "Комедия"
+        TR = "TR", "Трагедия"
+        DR = "DR", "Драма"
+        HO = "HO", "Ужасы"
+
+    name = models.TextField(verbose_name="Название публикации")
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    text = models.TextField(verbose_name="Содержимое публикации")
+    date = models.DateField(verbose_name="Дата публикации", auto_now=True)
+    genre = models.CharField(max_length=2, choices=Genres.choices, default=Genres.CO)
+    is_adult_only = models.BooleanField(
+        verbose_name="Только для взрослых?", default=False
+    )
