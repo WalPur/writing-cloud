0b67c17c168557e6393a19bdd7f2a44675146ff3
ichail <mishkamba1@gmail.com>
2023-03-15 14:48:00 +0900

    Footer added

--- frontend/src/pages/layout.jsx
+++ frontend/src/pages/layout.jsx
@@ -3,6 +3,8 @@ import { Outlet, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import { Search } from "react-bootstrap-icons";
 
@@ -46,6 +48,69 @@ const Layout = () => {
         </Container>
       </Navbar>
       <Outlet />
+      <footer>
+        <Container>
+          <Row>
+            <Col>
+              <img
+                src="/images/logo.svg"
+                className="footer-top-element"
+                alt=""
+              />
+              <p className="footer-text">О нас</p>
+              <p className="footer-text">Каталог</p>
+              <p className="footer-text">Условия использования</p>
+              <p className="footer-text">
+                Политика обработки персональных данных
+              </p>
+              <p className="footer-text">Новости</p>
+            </Col>
+            <Col>
+              <h2 className="footer-top-text footer-top-element">Компания</h2>
+              <p className="footer-text">Кто мы</p>
+              <p className="footer-text">Корпоративным клиентам</p>
+              <p className="footer-text">Контакты</p>
+              <p className="footer-text">Верификация</p>
+              <p className="footer-text">Политика плагиата</p>
+            </Col>
+            <Col>
+              <h2 className="footer-top-text footer-top-element">
+                Пресс центр
+              </h2>
+              <p className="footer-text">История</p>
+              <p className="footer-text">Миссия и ценности</p>
+              <p className="footer-text">Пресс релизы</p>
+              <p className="footer-text">СМИ о нас</p>
+              <p className="footer-text">Контакты для прессы</p>
+            </Col>
+            <Col>
+              <div className="phone">
+                <p className="phone-number footer-top-text">
+                  8 (000) 000-00-00
+                </p>
+                <p className="phone-text footer-text">Тех. поддержка</p>
+              </div>
+              <div className="phone">
+                <p className="phone-number footer-top-text">
+                  8 (000) 000-00-00
+                </p>
+                <p className="phone-text footer-text">Тех. поддержка</p>
+              </div>
+              <div className="phone">
+                <p className="phone-number footer-top-text">
+                  8 (000) 000-00-00
+                </p>
+                <p className="phone-text footer-text">Корпоративным клиентам</p>
+              </div>
+              <div className="socials">
+                <img src="/images/social_1.svg" alt="" />
+                <img src="/images/social_2.svg" alt="" />
+                <img src="/images/social_3.svg" alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
     </>
   );
 };
